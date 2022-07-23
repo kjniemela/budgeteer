bdd819905588fe9dd8f0d2bf468a4ac2bac26f3d
Johannes Niemelä <johannes@niemela.se>
2022-07-23 11:28:48 -0500

    added tabGroup component

---
+++ frontend/src/components/TabGroup.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class TabGroup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentTab: (Object.keys(props.tabs)[0] || null),
+    };
+  }
+
+  render() {
+    const { tabs } = this.props;
+    const { currentTab } = this.state;
+
+    return (
+      <>
+        <div className="horizontalBtnField">
+          {Object.keys(tabs).map(tab => (
+            <button className="solidBtn fullWidth" onClick={() => this.setState({ currentTab: tab })}>
+              {tab}
+            </button>
+          ))}
+        </div>
+        <hr />
+        {tabs[currentTab]}
+      </>
+    );
+  }
+}
+
+export default TabGroup;
